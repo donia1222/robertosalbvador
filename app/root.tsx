bc3f0f2c9fe3dec8c281a2150a32be3c3a5294af
lwev <106478077+lwev@users.noreply.github.com>
2026-01-02 15:25:15 +0100

    “update20“

--- app/root.tsx
+++ app/root.tsx
@@ -7,7 +7,7 @@ import {
   isRouteErrorResponse,
   useRouteError,
 } from "@remix-run/react";
-import type { LinksFunction, MetaFunction } from "@remix-run/node";
+import type { LinksFunction } from "@remix-run/node";
 
 import { ThemeProvider } from "~/context";
 import styles from "~/styles/global.css?url";
@@ -38,15 +38,12 @@ export const links: LinksFunction = () => [
   { rel: "stylesheet", href: styles },
 ];
 
-export const meta: MetaFunction = () => [
-  { charset: "utf-8" },
-  { name: "viewport", content: "width=device-width,initial-scale=1" },
-];
-
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="es" suppressHydrationWarning>
       <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
         <script dangerouslySetInnerHTML={{ __html: themeScript }} />
